6c3174c4bdd7af7bf1a7199e175426e39b5a3fa3
Haiqiang Jiang <jhq0113@163.com>
2021-03-06 18:27:49 +0800

    红黑树

--- redblacktree/main.go
+++ redblacktree/main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"crypto/md5"
 	"fmt"
-	"math/rand"
 	"time"
 )
 
@@ -70,7 +68,7 @@ func (rb *RbTree) Set(key, value interface{}) {
 				current.right = &Node{parent: current, Key: key, Value: value, color: red}
 				node = current.right
 				rb.size++
-				rb.insert(node, false)
+				rb.insert(node)
 				return
 			}
 			current = current.right
@@ -80,7 +78,7 @@ func (rb *RbTree) Set(key, value interface{}) {
 				node = current.left
 
 				rb.size++
-				rb.insert(node, true)
+				rb.insert(node)
 				return
 			}
 			current = current.left
@@ -88,48 +86,57 @@ func (rb *RbTree) Set(key, value interface{}) {
 	}
 }
 
-func (rb *RbTree) insert(node *Node, isLeft bool) {
+func (rb *RbTree) insert(node *Node) {
 	if isBlack(node.parent) {
 		return
 	}
 
 	//父节点为红色 -> 祖父节点一定为黑色，且一定存在(红色节点的孩子必须为黑色，根节点必须为黑色)
 	var (
-		reference *Node
+		uncle *Node
 	)
 
-	if isLeft { //插入左节点
-		reference = node.parent.parent.right
-		if !isBlack(reference) {
-			node.parent.color = black
-			reference.color = black
-			node.parent.parent.color = red
-			node = node.parent.parent
-		} else {
-			if node == node.parent.right {
-				node = node.parent
-				rb.leftRotate(node)
-			}
+	//循环判断父节点颜色，知道父节点为黑色的时候退出
+	for !isBlack(node.parent) {
+		//如果父节点为左节点
+		if node.parent == node.parent.parent.left {
+			//叔叔节点
+			uncle = node.parent.parent.right
+			if !isBlack(uncle) { //叔叔节点为红色
+				node.parent.color = black
+				uncle.color = black
+				node.parent.parent.color = red
+				node = node.parent.parent
+			} else { //叔叔节点为黑色
+				//当前节点在右，需要将当前节点变到左边
+				if node == node.parent.right {
+					node = node.parent
+					rb.leftRotate(node)
+				}
 
-			node.parent.color = black
-			node.parent.parent.color = red
-			rb.rightRotate(node.parent.parent)
-		}
-	} else { //插入右节点
-		reference = node.parent.parent.left
-		if !isBlack(reference) {
-			node.parent.color = black
-			reference.color = black
-			node.parent.parent.color = red
-			node = node.parent.parent
-		} else {
-			if node == node.parent.left {
-				node = node.parent
-				rb.rightRotate(node)
+				//当前节点在左
+				node.parent.color = black
+				node.parent.parent.color = red
+				rb.rightRotate(node.parent.parent)
 			}
-			node.parent.color = black
-			node.parent.parent.color = red
-			rb.leftRotate(node.parent.parent)
+		} else { //父节点为右节点
+			uncle = node.parent.parent.left
+			if !isBlack(uncle) { //叔叔节点为红色
+				node.parent.color = black
+				uncle.color = black
+				node.parent.parent.color = red
+				node = node.parent.parent
+			} else { //叔叔节点为黑色
+				//当前节点在左，需要将当前节点变到右面
+				if node == node.parent.left {
+					node = node.parent
+					rb.rightRotate(node)
+				}
+				//当前节点在右
+				node.parent.color = black
+				node.parent.parent.color = red
+				rb.leftRotate(node.parent.parent)
+			}
 		}
 	}
 
@@ -193,26 +200,104 @@ func (rb *RbTree) rightRotate(node *Node) {
 }
 
 func (rb *RbTree) Get(key interface{}) (value interface{}, exists bool) {
+	if node, ok := rb.GetNode(key); ok {
+		return node.Value, ok
+	}
+	return
+}
+
+func (rb *RbTree) GetNode(key interface{}) (node *Node, exists bool) {
 	var (
-		node = rb.root
+		current = rb.root
 	)
 
 	for {
-		if node == nil {
+		if current == nil {
 			return nil, false
 		}
-		switch rb.comparator(key, node.Key) {
+		switch rb.comparator(key, current.Key) {
 		case 0:
-			return node.Value, true
+			return current, true
 		case -1:
-			node = node.left
+			current = current.left
 		case 1:
-			node = node.right
+			current = current.right
 		}
 	}
 }
 
+//后继节点
+func (rb *RbTree) successor(node *Node) (successor *Node) {
+	//如果右节点存在
+	if node.right != nil {
+		//先假设右节点为后继节点
+		successor = node.right
+		//循环查找右节点的左节点，直到不存在左节点为止
+		for successor.left != nil {
+			successor = successor.left
+		}
+		return successor
+	}
+
+	var (
+		current = node
+	)
+	//右节点不存在，先假设父节点为后继节点
+	successor = current.parent
+	//循环查找当前节点的父节点，直到当前节点不为父节点的右节点为止
+	for successor != nil && current == successor.right {
+		current = successor
+		successor = current.parent
+	}
+	return successor
+}
+
+//前驱节点
+func (rb *RbTree) presuccessor(node *Node) (presuccessor *Node) {
+	//如果左节点存在
+	if node.left != nil {
+		//先假设左节点为前驱节点
+		presuccessor = node.left
+		//循环查找左节点的右节点，直到不存在右节点为止
+		for presuccessor.right != nil {
+			presuccessor = presuccessor.right
+		}
+		return presuccessor
+	}
+
+	//左节点不存在
+	//父节点存在
+	if node.parent != nil {
+		//当前节点为父节点的右节点
+		if node.parent.right == node {
+			return node.parent
+		}
+
+		//当前节点为父节点左节点
+		var (
+			current = node
+		)
+
+		//假设前驱节点为当前节点父节点
+		presuccessor = current.parent
+		//循环查找，直到当前节点不为父节点的左节点为止
+		for presuccessor != nil && presuccessor.left == current {
+			current = presuccessor
+			presuccessor = current.parent
+		}
+		return presuccessor
+	}
+
+	return presuccessor
+}
+
 func (rb *RbTree) Del(key interface{}) {
+	_, exists := rb.GetNode(key)
+	if !exists {
+		return
+	}
+
+	rb.size--
 
 }
 
@@ -220,58 +305,91 @@ func (rb *RbTree) Len() (length uint32) {
 	return rb.size
 }
 
-func (rb *RbTree) Range(func(key, value interface{}) (handled bool)) {
+//非递归中序遍历
+func (rb *RbTree) Range(handler func(key, value interface{}) (handled bool)) {
+	if rb.root == nil {
+		return
+	}
 
+	var (
+		min = rb.root
+	)
+
+	for min.left != nil {
+		min = min.left
+	}
+
+	if handler(min.Key, min.Value) {
+		return
+	}
+
+	for {
+		node := rb.successor(min)
+		min = node
+		if node == nil {
+			return
+		}
+
+		if handler(node.Key, node.Value) {
+			return
+		}
+	}
+}
+
+//非递归倒序遍历
+func (rb *RbTree) Desc(handler func(key, value interface{}) (handled bool)) {
+	if rb.root == nil {
+		return
+	}
+
+	var (
+		max = rb.root
+	)
+
+	for max.right != nil {
+		max = max.right
+	}
+
+	if handler(max.Key, max.Value) {
+		return
+	}
+
+	for {
+		node := rb.presuccessor(max)
+		max = node
+		if node == nil {
+			return
+		}
+
+		if handler(node.Key, node.Value) {
+			return
+		}
+	}
 }
 
 func main() {
 	rbTree := NewRbTree(func(key1, key2 interface{}) int8 {
-		s1 := key1.(string)
-		s2 := key2.(string)
-		min := len(s2)
-		if len(s1) < len(s2) {
-			min = len(s1)
-		}
-
-		diff := 0
-		for i := 0; i < min && diff == 0; i++ {
-			diff = int(s1[i]) - int(s2[i])
-		}
-
-		if diff == 0 {
-			diff = len(s1) - len(s2)
-		}
-
-		if diff < 0 {
-			return -1
-		}
-
-		if diff > 0 {
+		diff := key1.(int64) - key2.(int64)
+		switch {
+		case diff > 0:
 			return 1
+		case diff < 0:
+			return -1
 		}
 		return 0
 	})
 
-	rand.Seed(time.Now().UnixNano())
-
 	var (
-		max = 1000000
+		index int64 = 1
+		max   int64 = 1000
 	)
 
-	start := time.Now()
-	keys := make([]string, max, max)
-	for index := 0; index < max; index++ {
-		current := time.Now()
-		keys[index] = fmt.Sprintf("%x", md5.Sum([]byte(current.String())))
-		rbTree.Set(keys[index], current.UnixNano())
+	for ; index < max; index++ {
+		rbTree.Set(index, time.Now().UnixNano())
 	}
 
-	fmt.Println("set cost:", time.Now().Sub(start))
-	start = time.Now()
-	for _, key := range keys {
-		if _, exist := rbTree.Get(key); !exist {
-			fmt.Println("err key:", key)
-		}
-	}
-	fmt.Println("get cost:", time.Now().Sub(start))
+	rbTree.Desc(func(key, value interface{}) (handled bool) {
+		fmt.Println(key, value)
+		return
+	})
 }
